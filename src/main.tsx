ea43d0d61106680b98cfb6836b38bac76e66ebf4
tHyt-lab <washers.fezzes-0q@icloud.com>
2023-06-09 15:32:04 +0000

    feat: add AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
---
+++ src/providers/app.tsx
@@ -0,0 +1,38 @@
+import { Box, Button, Loader } from "@mantine/core";
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
+
+const ErrorFallback = () => {
+  return (
+    <Box role="alert">
+      <Box component="h2">Ooops, something went wrong :( </Box>
+      <Button onClick={() => window.location.assign(window.location.origin)}>
+        Refresh
+      </Button>
+    </Box>
+  );
+};
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return (
+    <React.Suspense
+      fallback={
+        <Box>
+          <Loader size="md" />
+        </Box>
+      }
+    >
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <HelmetProvider>
+          <BrowserRouter>{children}</BrowserRouter>
+        </HelmetProvider>
+      </ErrorBoundary>
+    </React.Suspense>
+  );
+};
